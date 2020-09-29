1e1bf6ae5db1fd666fd85f4191e97bb249eb3f89
xido <1661480468@qq.com>
2020-09-29 23:53:28 +0800

    9.29

--- answer/1512.js
+++ answer/1512.js
@@ -7,7 +7,7 @@ var numIdenticalPairs = function(nums) {
     let x = 0;
     for (let i = 0; i < nums.length - 1; i++) {
         for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] == nums[j]) {
+            if (nums[i] === nums[j]) {
                 x++;
             }
         }
